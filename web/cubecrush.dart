82cbd6e97956e39ef5e48ea1899a3779a7e2237b
leafinity <radleaf@gmail.com>
2013-09-19 18:14:54 +0800

    layout

--- web/cubecrush.dart
+++ web/cubecrush.dart
@@ -7,6 +7,8 @@ Random random = new Random();
 
 void main() { 
   parent = query('#frame');
+  query('.copyright').style.top = "${window.innerHeight - 120}px";
+  query('#outBorder').style.left = "${(window.innerWidth - 952) / 2}px";
   createBlocks();
   query('#start').onClick.listen((MouseEvent event) {
     startHtml();
